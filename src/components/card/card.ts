34d26cae72561cd42cee2926154841deb7c70efc
cboenke <clara.boenke@web.de>
2021-02-11 12:58:06 +0100

    :art: Improve structure of the code as preparation

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -4,29 +4,50 @@ export function createCard({ imgSrc, name, status, species, origin }) {
   return createElement("article", {
     className: "card",
     childs: [
-      createElement("img", {
-        className: "card__portrait",
-        src: imgSrc,
-        alt: "",
-      }),
       createElement("div", {
-        className: "card__info",
+        className: "card__inner",
         childs: [
-          createElement("h2", {
-            className: "card__name",
-            innerText: name,
+          createElement("div", {
+            className: "card__front",
+            childs: [
+              createElement("img", {
+                className: "card__portrait",
+                src: imgSrc,
+                alt: "",
+              }),
+              createElement("div", {
+                className: "card__info",
+                childs: [
+                  createElement("h2", {
+                    className: "card__name",
+                    innerText: name,
+                  }),
+                  createElement("p", {
+                    className: "card__status",
+                    innerText: `${
+                      status === "Alive" ? "🥳" : "💀"
+                    } - ${status}`,
+                  }),
+                  createElement("p", {
+                    className: "card__species",
+                    innerText: species,
+                  }),
+                  createElement("p", {
+                    className: "card__origin",
+                    innerText: origin.name,
+                  }),
+                ],
+              }),
+            ],
           }),
-          createElement("p", {
-            className: "card__status",
-            innerText: `${status === "Alive" ? "🥳" : "💀"} - ${status}`,
-          }),
-          createElement("p", {
-            className: "card__species",
-            innerText: species,
-          }),
-          createElement("p", {
-            className: "card__origin",
-            innerText: origin.name,
+          createElement("div", {
+            className: "card__back",
+            childs: [
+              createElement("p", {
+                className: "card__back__inner",
+                innerText: "Location",
+              }),
+            ],
           }),
         ],
       }),
